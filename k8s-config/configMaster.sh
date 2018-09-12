8ce37384b8faf7c55697ab82ed0ce4d2d2f91fa0
Carlos Ravelo <ravelo.carlos@gmail.com>
2018-09-11 21:55:35 -0400

    Added hostpath provisioner and ingress controller, some fixes

--- k8s-config/configMaster.sh
+++ k8s-config/configMaster.sh
@@ -8,19 +8,19 @@ fi
 echo "Upgrade ========================================================================================================"
 sudo dnf -y update || exit 1
 
-echo "\nGet docker ====================================================================================================="
+echo "\nGet docker ==================================================================================================="
 if [ ! -f docker-ce-17.03.2.ce-1.fc25.x86_64.rpm ]; then
     wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
     wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
 
-    echo "\nInstall docker & deps ======================================================================================"
+    echo "\nInstall docker & deps ========================================================================================"
     sudo dnf -y install policycoreutils-python || exit 1
     sudo mkdir -p /var/lib/docker || exit 1
     sudo rpm -i docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
     sudo rpm -i docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
 fi;
 
-echo "\nEnable docker =================================================================================================="
+echo "\nEnable docker ================================================================================================"
 sudo systemctl enable --now docker || exit 1
 
 echo "\nVerify docker is working by running hello-world"
@@ -54,51 +54,66 @@ sudo dnf -y install --enablerepo=kubernetes kubelet kubectl --disableexcludes=ku
 sudo dnf -y install --enablerepo=kubernetes-unstable kubeadm --disableexcludes=kubernetes || exit 1
 sudo dnf -y install cockpit-docker cockpit-kubernetes git || exit 1
 
-echo "\nEnabling cockpit ==============================================================================================="
+echo "\nEnabling cockpit ============================================================================================="
 sudo systemctl enable --now cockpit.socket || exit 1
 
-echo "\nDisabling swap ================================================================================================="
+echo "\nDisabling swap ==============================================================================================="
 sudo swapoff -a || exit 1
 
-echo "\nDisabling the firewall ========================================================================================="
+echo "\nDisabling the firewall ======================================================================================="
 sudo systemctl stop firewalld || exit 1
 sudo systemctl disable firewalld || exit 1
 setenforce 0
 
-echo "\nInstalling kubernetes =========================================================================================="
-sudo systemctl enable kubelet.service
-sudo kubeadm config images pull
-sudo kubeadm init --config=./kubeadm.yaml || exit 1
+echo "\nInstalling kubernetes ========================================================================================"
+if [ ! -f "kubeadminit.lock" ]; then
+    sudo systemctl enable kubelet.service
+    sudo kubeadm config images pull
+    sudo kubeadm init --config=./kubeadm.yaml || exit 1
+    touch kubeadminit.lock
+else
+    echo "\nNOTE: Looks like kubeadm init already ran. If you want to run it again, delete kubeadminit.lock =========="
+fi;
 
-echo "\nCopy kubectl config ========================================================================================"
+echo "\nCopy kubectl config =========================================================================================="
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "\nVerify that is running: ========================================================================================"
+echo "\nVerify that is running: ======================================================================================"
 kubectl get nodes
 kubectl get all --all-namespaces
 sudo systemctl status kubelet
 
-echo "\nInstalling flannel ============================================================================================="
+echo "\nInstalling flannel ==========================================================================================="
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
 
-echo "\nLetting the master node run pods ==============================================================================="
+echo "\nLetting the master node run pods ============================================================================="
 MASTER_HOSTNAME=`kubectl get nodes -o jsonpath='{.items[0].metadata.name}'`
 kubectl taint node $MASTER_HOSTNAME node-role.kubernetes.io/master:NoSchedule-
 
-echo "\nInstalling the dashboard ======================================================================================="
+echo "\nInstalling Hostpath Provisioner =============================================================================="
+kubectl apply -f ./hostpath.yaml || exit 1
+
+MASTER_SERVER=`kubectl config view -o jsonpath='{.clusters[0].cluster.server}'`;
+MASTER_IP=`echo "${MASTER_SERVER}" | awk -F: '{print $2}' | awk -F/ '{print $3}'`
+
+echo "\nInstalling Ingress controller ================================================================================"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f ./ingress.yaml
+kubectl patch service ingress-nginx --namespace=ingress-nginx --patch '{"spec": {"externalIPs": ["'${MASTER_IP}'"]}}'
+
+echo "\nInstalling the dashboard ====================================================================================="
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
 kubectl apply -f ./dashboard.yaml || exit 1
+#kubectl patch service kubernetes-dashboard --namespace=kube-system --patch '{"spec": {"externalIPs": ["'${MASTER_IP}'"]}}'
 DASHBOARD_USER_SECRET=`kubectl -n kube-system get secret | grep admin-user | awk '{print $1}'`
 if [ ! -z "$DASHBOARD_USER_SECRET" ]; then
-    MASTER_SERVER=`kubectl config view -o jsonpath='{.clusters[0].cluster.server}'`;
-    DASHBOARD_URL=`echo "https://10.1.1.63:6443" | awk -F: '{print $2}'`
-    echo "\nDashboard installed! visit: https:$DASHBOARD_URL:8443\n"
+    echo "\nDashboard installed! visit: https://$MASTER_IP:8443/\n"
     kubectl -n kube-system describe secret ${DASHBOARD_USER_SECRET}
 else
-    echo "\nSomething went wrong getting the credentials for the dashboard"
+    echo "\nSomething went wrong getting the credentials for the dashboard\n"
 fi;
 
 ./installHelm.sh
