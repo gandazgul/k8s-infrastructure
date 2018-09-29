16968309fc9056d1e4a6112ce974fc77a1ec5e51
Carlos Ravelo <ravelo.carlos@gmail.com>
2018-09-28 22:49:03 -0400

    updates to plex and seedbox

--- docker/openvpn-transmission-flexget/etc/openvpn/tunnelUp.sh
+++ docker/openvpn-transmission-flexget/etc/openvpn/tunnelUp.sh
@@ -5,14 +5,9 @@
 /etc/transmission/start.sh "$@"
 [ ! -f /opt/tinyproxy/start.sh ] || /opt/tinyproxy/start.sh
 
-# `flexget daemon` runs all tasks in the config every hour by default, for more
-# options see https://flexget.com/Plugins/Daemon/scheduler.
-#
 # `--autoreload-config` reloads the config *before* running the tasks, not
 # when the configuration actually changes.
 #
-
-#s6-setuidgid abc /usr/bin/flexget \
-#    -c /config/flexget/config.yml \
-#    --loglevel "${FLEXGET_LOG_LEVEL:-debug}" \
-#    daemon start --autoreload-config
+/usr/bin/flexget \
+    -c /config/flexget/config.yml \
+    daemon start -d --autoreload-config
