520a2ef649c0492b324abd9eae9807a607fc8c1d
Carlos Ravelo <ravelo.carlos@gmail.com>
2018-09-30 19:04:24 -0400

    Better prompt and optional bash_history for seedbox

--- k8s-config/2-configK8SMaster.sh
+++ k8s-config/2-configK8SMaster.sh
@@ -1,71 +1,6 @@
 #!/bin/bash
 
-if [[ $EUID = 0 ]]; then
-   printf "Don't run this script as root"
-   exit 1
-fi
-
-printf "Upgrade ====================================================================================================\n"
-sudo dnf -y update || exit 1
-
-printf "\nGet docker ===============================================================================================\n"
-if [ ! -f docker-ce-17.03.2.ce-1.fc25.x86_64.rpm ]; then
-    wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
-    wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
-
-    printf "\nInstall docker & deps ====================================================================================\n"
-    sudo dnf -y install policycoreutils-python || exit 1
-    sudo mkdir -p /var/lib/docker || exit 1
-    sudo rpm -i docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
-    sudo rpm -i docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
-fi;
-
-printf "\nEnable docker ============================================================================================\n"
-sudo systemctl enable --now docker || exit 1
-
-printf "\nVerify docker is working by running hello-world"
-sudo docker run --rm hello-world
-
-if [ ! -f /etc/yum.repos.d/kubernetes.repo ]; then
-    printf "\nInstall kubelet, kubeadm, crictl(needed by kubelet), cockpit (nice fedora dashboard):"
-    sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kube*
-
-[kubernetes-unstable]
-name=Kubernetes-unstable
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64-unstable
-enabled=0
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kube*
-EOF'
-fi;
-
-sudo setenforce 0
-sudo dnf -y install --enablerepo=kubernetes kubelet kubectl --disableexcludes=kubernetes || exit 1
-sudo dnf -y install --enablerepo=kubernetes-unstable kubeadm --disableexcludes=kubernetes || exit 1
-sudo dnf -y install cockpit-docker cockpit-kubernetes || exit 1
-
-printf "\nEnabling cockpit =========================================================================================\n"
-sudo systemctl enable --now cockpit.socket || exit 1
-
-printf "\nDisabling swap ===========================================================================================\n"
-sudo swapoff -a || exit 1
-
-printf "\nDisabling the firewall ===================================================================================\n"
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-sudo dnf -y remove firewalld
-sudo setenforce 0
-echo "SELINUX=disabled" | sudo tee -a /etc/sysconfig/selinux
+./configNode.sh
 
 printf "\nInstalling kubernetes ====================================================================================\n"
 if [ ! -f "kubeadminit.lock" ]; then
@@ -90,5 +25,5 @@ printf "\n\n====================================================================
 printf "Kubernetes is now installed. Please check the status of flannel and kubelet to make sure the network is ready before we proceed to the next step."
 printf "\nVerify that is running:\n"
 kubectl get nodes
-kubectl get all --all-namespaces
-printf "Run 'sudo systemctl status kubelet' to see how kubelet is doing"
+kubectl get ds --namespace=kube-system
+printf "Run 'sudo systemctl status kubelet' to see how kubelet is doing\n\n"
---
+++ k8s-config/configNode.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [[ $EUID = 0 ]]; then
+   printf "Don't run this script as root"
+   exit 1
+fi
+
+printf "\nGet docker ===============================================================================================\n"
+if [ ! -f docker-ce-17.03.2.ce-1.fc25.x86_64.rpm ]; then
+    wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
+    wget https://download.docker.com/linux/fedora/25/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
+
+    printf "\nInstall docker & deps ====================================================================================\n"
+    sudo dnf -y install policycoreutils-python || exit 1
+    sudo mkdir -p /var/lib/docker || exit 1
+    sudo rpm -i docker-ce-selinux-17.03.2.ce-1.fc25.noarch.rpm || exit 1
+    sudo rpm -i docker-ce-17.03.2.ce-1.fc25.x86_64.rpm || exit 1
+fi;
+
+printf "\nEnable docker ============================================================================================\n"
+sudo systemctl enable --now docker || exit 1
+
+printf "\nVerify docker is working by running hello-world ==========================================================\n"
+sudo docker run --rm hello-world || exit 1
+
+printf "\nDisable SELINUX because kubelet doesnt support it ========================================================\n"
+sudo setenforce 0
+echo "SELINUX=disabled" | sudo tee /etc/sysconfig/selinux
+
+if [ ! -f /etc/yum.repos.d/kubernetes.repo ]; then
+    printf "\nInstall kubelet, kubeadm, crictl(needed by kubelet), cockpit (nice fedora dashboard):"
+    sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+
+[kubernetes-unstable]
+name=Kubernetes-unstable
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64-unstable
+enabled=0
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF'
+fi;
+
+sudo dnf -y install --enablerepo=kubernetes kubelet kubectl kubeadm --disableexcludes=kubernetes || exit 1
+sudo dnf -y install cockpit-docker cockpit-kubernetes cockpit-pcp || exit 1
+
+printf "\nEnabling cockpit =========================================================================================\n"
+sudo systemctl enable --now cockpit.socket || exit 1
+
+printf "\nDisabling swap ===========================================================================================\n"
+sudo swapoff -a || exit 1
+
+printf "\nDisabling the firewall ===================================================================================\n"
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo dnf -y remove firewalld
