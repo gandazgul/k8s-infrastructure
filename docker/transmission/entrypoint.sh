2a2de0dd589b7296fe6394417a9489a2542343c9
Carlos Ravelo <ravelo.carlos@gmail.com>
2020-11-28 00:31:18 -0500

    Using linuxserver's transmission container instead of my own. Updated PIA's container version
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

--- docker/transmission/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# start crond to run the transmission port cronjob
-touch /var/log/crond.log
-crond -L /var/log/crond.log
-
-# Call the original entry point
-su -s /bin/bash abc -c \
-  "/usr/bin/transmission-daemon --foreground -g /data --port ${RPC_PORT}"
