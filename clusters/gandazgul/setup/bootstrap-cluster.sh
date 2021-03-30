4ca0b90d708eb4c2306252a03e3ff93afde9be59
Carlos Ravelo <ravelo.carlos@gmail.com>
2021-03-29 23:15:48 -0400

    Fixed bootstrap-cluster.sh hopefully fix secrets
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

--- clusters/gandazgul/setup/bootstrap-cluster.sh
+++ clusters/gandazgul/setup/bootstrap-cluster.sh
@@ -1,33 +1,11 @@
 #!/usr/bin/env bash
 
 need() {
-    if ! command -v "$1" &> /dev/null
-    then
-        echo "Binary '$1' is missing but required"
-        exit 1
-    fi
+  if ! command -v "$1" &>/dev/null; then
+    echo "Binary '$1' is missing but required"
+    exit 1
+  fi
 }
-
-need "kubectl"
-need "helm"
-need "flux"
-need "git"
-
-REPO_ROOT=$(git rev-parse --show-toplevel)
-# shellcheck source=clusters/gandazgul/setup/secrets.env
-. "$REPO_ROOT"clusters/gandazgul/setup/secrets.env
-
-# Master node information
-MASTER_IP=`kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.annotations.flannel\.alpha\.coreos\.com\/public-ip}'`
-# TODO: automatically update secrets.env?
-MASTER_NODE_NAME=`kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}'`
-# TODO: automatically update secrets.env?
-
-echo "Using MASTER_IP=$MASTER_IP"
-echo "Using MASTER_NODE_NAME=$MASTER_NODE_NAME"
-echo "Using INGRESS_INTERNAL_NAME=$INGRESS_INTERNAL_NAME"
-echo "Using INGRESS_EXTERNAL_NAME=$INGRESS_EXTERNAL_NAME"
-pause
 
 message() {
   echo -e "\n######################################################################"
@@ -35,9 +13,35 @@ message() {
   echo "######################################################################"
 }
 
+pause() {
+  read -r -s -n 1 -p "Check these values. If anything looks wrong stop now and check the secrets.env file. Press any key to continue . . ."
+  echo ""
+}
+
+need "kubectl"
+need "helm"
+need "flux"
+need "git"
+
 installFlux() {
+  REPO_ROOT=$(git rev-parse --show-toplevel)
+  # shellcheck source=clusters/gandazgul/setup/secrets.env
+  export $(cat "$REPO_ROOT"/clusters/gandazgul/setup/secrets.env | sed 's/#.*//g' | xargs)
+
+  # Master node information
+  MASTER_IP=$(kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.annotations.flannel\.alpha\.coreos\.com\/public-ip}')
+  # TODO: automatically update secrets.env?
+  MASTER_NODE_NAME=$(kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}')
+  # TODO: automatically update secrets.env?
+
+  echo "Using MASTER_IP=$MASTER_IP"
+  echo "Using MASTER_NODE_NAME=$MASTER_NODE_NAME"
+  echo "Using INGRESS_INTERNAL_NAME=$INGRESS_INTERNAL_NAME"
+  echo "Using INGRESS_EXTERNAL_NAME=$INGRESS_EXTERNAL_NAME"
+  pause
+
   message "installing fluxv2"
-  flux check --pre > /dev/null
+  flux check --pre >/dev/null
   FLUX_PRE=$?
   if [ $FLUX_PRE != 0 ]; then
     echo -e "flux prereqs not met:\n"
@@ -45,14 +49,14 @@ installFlux() {
     exit 1
   fi
   if [ -z "$GITHUB_TOKEN" ]; then
-    echo "GITHUB_TOKEN is not set! Check $REPO_ROOT/setup/secrets.env"
+    echo "GITHUB_TOKEN is not set! Check $REPO_ROOT/clusters/gandazgul/setup/secrets.env"
     exit 1
   fi
   flux bootstrap github \
     --owner="$GITHUB_USER" \
     --repository=k8s-infrastructure \
     --branch=feature/gitops \
-    --path=./clusters/gandazgul \
+    --path=.clusters/gandazgul/flux/ \
     --personal
 
   FLUX_INSTALLED=$?
