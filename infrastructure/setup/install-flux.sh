258a4a8e14af17a956c8d261526bd0c7b9c2332b
Carlos Ravelo <ravelo.carlos@gmail.com>
2021-09-05 00:04:38 -0400

    Adding a kustomization file to secrets folder
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

--- infrastructure/setup/install-flux.sh
+++ infrastructure/setup/install-flux.sh
@@ -73,11 +73,18 @@ configureCluster() {
   echo "Generating $CLUSTER_NAME app secrets from values..."
   rm -rf "./clusters/$CLUSTER_NAME/apps/secrets/"
   mkdir "./clusters/$CLUSTER_NAME/apps/secrets/"
+  cat <<EOT >> "./clusters/$CLUSTER_NAME/apps/secrets/kustomization.yaml"
+---
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+EOT
 
   for f in ./clusters/"$CLUSTER_NAME"/apps/values/*.yaml; do
     echo "Generating secrets from values file: $f..."
     basename=$(basename "$f" .yaml)
     kubectl create secret generic "${basename}" --dry-run=client --from-file=values.yaml="${f}" -o yaml >"./clusters/$CLUSTER_NAME/apps/secrets/${basename}.yaml"
+    echo "- ${basename}.yaml" >> "./clusters/$CLUSTER_NAME/apps/secrets/kustomization.yaml"
   done
 
   kubectl apply -f "./clusters/$CLUSTER_NAME/ClusterKustomization.yaml"
