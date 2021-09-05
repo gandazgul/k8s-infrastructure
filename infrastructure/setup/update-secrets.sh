527ac7c47e5eb2dae6648de467b32c1c76e0c08b
Carlos Ravelo <ravelo.carlos@gmail.com>
2021-09-04 21:58:53 -0400

    simplified the cluster folder
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

--- infrastructure/setup/update-secrets.sh
+++ infrastructure/setup/update-secrets.sh
@@ -3,13 +3,13 @@
 for cluster in ./clusters/*/ ; do
     echo "Processing $cluster"
     # Seal main secrets file
-    rm -rf "$cluster"sealed-secrets/SealedSecret.yaml
+    rm -rf "$cluster"SealedSecret.yaml
     kubectl create secret generic secrets --dry-run=client --namespace=flux-system --from-env-file="$cluster"secrets.env -o json \
-     | kubeseal -o yaml > "$cluster"sealed-secrets/SealedSecret.yaml
+     | kubeseal -o yaml > "$cluster"SealedSecret.yaml
     # apply it
-    kubectl apply -f "$cluster"sealed-secrets/SealedSecret.yaml
-    flux reconcile kustomization kube-system
-    flux reconcile kustomization apps
+    kubectl apply -f "$cluster"SealedSecret.yaml
+    clusterName="$(basename $cluster)"
+    flux reconcile kustomization "$clusterName"
 
     # Create value/yaml secrets
     rm -rf "$cluster"apps/secrets/
