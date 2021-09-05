4f45aa7bc6b95c20342895a9a876db543610760e
Carlos Ravelo <ravelo.carlos@gmail.com>
2021-09-04 23:32:06 -0400

    updated install scripts moved the Dashboard to the apps
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

--- infrastructure/setup/install-flux.sh
+++ infrastructure/setup/install-flux.sh
@@ -18,13 +18,23 @@ pause() {
   echo ""
 }
 
-need "kubectl"
-need "helm"
-need "flux"
-need "git"
-
 installFlux() {
   REPO_ROOT=$(git rev-parse --show-toplevel)
+
+  # Master node information
+  MASTER_IP=$(kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.annotations.flannel\.alpha\.coreos\.com\/public-ip}')
+  MASTER_NODE_NAME=$(kubectl get nodes --selector=node-role.kubernetes.io/master -o=jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}')
+
+  # Confirm information
+  echo "Using MASTER_IP=$MASTER_IP"
+  echo "Using MASTER_NODE_NAME=$MASTER_NODE_NAME"
+  echo "Using CLUSTER_NAME=$CLUSTER_NAME"
+  pause
+
+  # Update secrets file
+  secretFile="$REPO_ROOT/clusters/$CLUSTER_NAME/secrets.env"
+  sed -i '' "s/\(MASTER_IP=*\).*/\1$MASTER_IP/" "$secretFile" || exit 1
+  sed -i '' "s/\(MASTER_NODE_NAME=*\).*/\1$MASTER_NODE_NAME/" "$secretFile" || exit 1
 
   message "installing Flux v2"
   flux check --pre >/dev/null
@@ -35,6 +45,7 @@ installFlux() {
     exit 1
   fi
 
+  message "Bootstrapping cluster with Flex components..."
   kubectl apply -k "$REPO_ROOT"/infrastructure/flux-system/components/
 
   FLUX_INSTALLED=$?
@@ -44,18 +55,55 @@ installFlux() {
   fi
 }
 
+configureCluster() {
+  message "Installing $CLUSTER_NAME configs..."
+
+  echo "Generating $CLUSTER_NAME secret..."
+  # Seal main secrets file
+  rm -rf "./clusters/$CLUSTER_NAME/sealed-secret/SealedSecret.yaml"
+  kubectl create secret generic secrets --dry-run=client --namespace=flux-system --from-env-file="./clusters/$CLUSTER_NAME/secrets.env" -o json |
+    kubeseal -o yaml >"./clusters/$CLUSTER_NAME/sealed-secret/SealedSecret.yaml"
+
+  # apply it
+  template=$(sed "s/{{CLUSTER_NAME}}/$CLUSTER_NAME/g" <"$REPO_ROOT"/infrastructure/setup/SealedSecretsKustomization.yaml.templ)
+  # apply the yml with the substituted value
+  echo "$template" | kubectl apply -f - || exit 1
+
+  # Create value/yaml secrets
+  echo "Generating $CLUSTER_NAME app secrets from values..."
+  rm -rf "./clusters/$CLUSTER_NAME/apps/secrets/"
+  mkdir "./clusters/$CLUSTER_NAME/apps/secrets/"
+
+  for f in ./clusters/"$CLUSTER_NAME"/apps/values/*.yaml; do
+    echo "Generating secrets from values file: $f..."
+    basename=$(basename "$f" .yaml)
+    kubectl create secret generic "${basename}" --dry-run=client --from-file=values.yaml="${f}" -o yaml >"./clusters/$CLUSTER_NAME/apps/secrets/${basename}.yaml"
+  done
+
+  kubectl apply -f "./clusters/$CLUSTER_NAME/ClusterKustomization.yaml"
+
+  echo "Done configuring $CLUSTER_NAME's cluster"
+}
+
 if [ -z ${1+x} ]; then
   echo "Make sure to pass in a cluster name as install-flux.sh [name here]"
   exit 1
-fi;
+fi
+
+need "kubectl"
+need "helm"
+need "flux"
+need "git"
+
+CLUSTER_NAME=$1
 
 installFlux
 # wait for secrets controller to be available
-while : ; do
+while :; do
   kubectl get svc sealed-secrets-controller -n kube-system && break
   sleep 5
 done
 
-"$REPO_ROOT"/infrastructure/setup/bootstrap-cluster.sh "$1"
+configureCluster
 
 message "all done!"
