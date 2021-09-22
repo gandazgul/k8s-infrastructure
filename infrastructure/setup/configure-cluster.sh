9632a3d33e7f1ff3d10983028cdcc1474e59d781
Carlos Ravelo <ravelo.carlos@gmail.com>
2021-09-22 00:22:32 -0400

    Split out script to update secrets so it may be run when needed to update values
    Signed-off-by: Carlos Ravelo <ravelo.carlos@gmail.com>

---
+++ infrastructure/setup/configure-cluster.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME=$1
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+message "Installing $CLUSTER_NAME configs..."
+
+echo "Generating $CLUSTER_NAME secret..."
+# Seal main secrets file
+rm -rf "./clusters/$CLUSTER_NAME/sealed-secret/SealedSecret.yaml"
+kubectl create secret generic secrets --dry-run=client --namespace=flux-system --from-env-file="./clusters/$CLUSTER_NAME/secrets.env" -o json |
+  kubeseal -o yaml >"./clusters/$CLUSTER_NAME/sealed-secret/SealedSecret.yaml"
+
+# apply it
+template=$(sed "s/{{CLUSTER_NAME}}/$CLUSTER_NAME/g" <"$REPO_ROOT"/infrastructure/setup/SealedSecretsKustomization.yaml.templ)
+# apply the yml with the substituted value
+echo "$template" | kubectl apply -f - || exit 1
+
+# Create value/yaml secrets
+echo "Generating $CLUSTER_NAME app secrets from values..."
+rm -rf "./clusters/$CLUSTER_NAME/apps/secrets/"
+mkdir "./clusters/$CLUSTER_NAME/apps/secrets/"
+cat <<EOT >> "./clusters/$CLUSTER_NAME/apps/secrets/kustomization.yaml"
+---
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+EOT
+
+for f in ./clusters/"$CLUSTER_NAME"/apps/values/*.yaml; do
+  echo "Generating secrets from values file: $f..."
+  basename=$(basename "$f" .yaml)
+  kubectl -n default create secret generic "${basename}" --dry-run=client --from-file=values.yaml="${f}" -o yaml >"./clusters/$CLUSTER_NAME/apps/secrets/${basename}.yaml"
+  echo "- ${basename}.yaml" >> "./clusters/$CLUSTER_NAME/apps/secrets/kustomization.yaml"
+done
+
+kubectl apply -f "./clusters/$CLUSTER_NAME/ClusterKustomization.yaml"
+
+echo "Done configuring $CLUSTER_NAME's cluster"
