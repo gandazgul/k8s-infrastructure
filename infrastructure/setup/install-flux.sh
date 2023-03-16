4c0607b3e68e5d69efe6d84f151948f620a14509
Carlos Ravelo <carlos.ravelo@ask.com>
2023-03-15 22:39:40 -0400

    Adding more logs to the install scripts
    Signed-off-by: Carlos Ravelo <carlos.ravelo@ask.com>

--- infrastructure/setup/install-flux.sh
+++ infrastructure/setup/install-flux.sh
@@ -1,61 +1,59 @@
 #!/usr/bin/env bash
 
+# check if have the cluster name
 if [ -z ${1+x} ]; then
-  echo "Make sure to pass in a cluster name as install-flux.sh [name here]"
+  echo "Make sure to pass in a cluster name like this: install-flux.sh [name here]"
   exit 1
 fi
 
 CLUSTER_NAME=$1
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Check that all required binaries are installed
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$SCRIPT_DIR/requirements.sh"
 
-installFlux() {
-  REPO_ROOT=$(git rev-parse --show-toplevel)
-
-  # Control plane node information
-  CONTROL_PLANE_IP=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o=jsonpath='{.items[0].metadata.annotations.flannel\.alpha\.coreos\.com\/public-ip}')
-  CONTROL_PLANE_NAME=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o=jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}')
-
-  # Confirm information
-  echo "Using CONTROL_PLANE_IP=$CONTROL_PLANE_IP"
-  echo "Using CONTROL_PLANE_NAME=$CONTROL_PLANE_NAME"
-  echo "Using CLUSTER_NAME=$CLUSTER_NAME"
-  pause
-
-  # Update secrets file
-  secretFile="$REPO_ROOT/clusters/$CLUSTER_NAME/secrets.env"
-  sed -i '' "s/\(CONTROL_PLANE_IP=*\).*/\1$CONTROL_PLANE_IP/" "$secretFile" || exit 1
-  sed -i '' "s/\(CONTROL_PLANE_NAME=*\).*/\1$CONTROL_PLANE_NAME/" "$secretFile" || exit 1
-
-  message "installing Flux v2"
-
-  if ! flux check --pre; then
-    echo -e "Flux prereqs not met see above..."
-    exit 1
-  fi
-
-  if ! flux install --components-extra=image-reflector-controller,image-automation-controller; then
-    echo -e "Flux did not install correctly, aborting!"
-    exit 1
-  fi
-
-  if ! kubectl apply -f "$REPO_ROOT"/infrastructure/setup/GitRepoSync.yaml; then
-    echo -e "Flux did not install correctly, aborting!"
-    exit 1
-  fi
-}
-
 ######################## Install Flux ###################################
-installFlux
-# wait for secrets controller to be available
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Control plane node information
+CONTROL_PLANE_IP=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o=jsonpath='{.items[0].metadata.annotations.flannel\.alpha\.coreos\.com\/public-ip}')
+CONTROL_PLANE_NAME=$(kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o=jsonpath='{.items[0].metadata.labels.kubernetes\.io/hostname}')
+
+# Confirm information
+echo "Using CONTROL_PLANE_IP=$CONTROL_PLANE_IP"
+echo "Using CONTROL_PLANE_NAME=$CONTROL_PLANE_NAME"
+echo "Using CLUSTER_NAME=$CLUSTER_NAME"
+pause
+
+# Update secrets file
+secretFile="$REPO_ROOT/clusters/$CLUSTER_NAME/secrets.env"
+sed -i '' "s/\(CONTROL_PLANE_IP=*\).*/\1$CONTROL_PLANE_IP/" "$secretFile" || exit 1
+sed -i '' "s/\(CONTROL_PLANE_NAME=*\).*/\1$CONTROL_PLANE_NAME/" "$secretFile" || exit 1
+
+message "Installing Flux v2"
+
+if ! flux check --pre; then
+  echo -e "Flux pre-reqs not met see above..."
+  exit 1
+fi
+
+if ! flux install --components-extra=image-reflector-controller,image-automation-controller; then
+  echo -e "Flux did not install correctly, aborting!"
+  exit 1
+fi
+
+message "Installing the Git Repo Source"
+if ! kubectl apply -f "$REPO_ROOT"/infrastructure/setup/GitRepoSync.yaml; then
+  echo -e "Flux did not install correctly, aborting!"
+  exit 1
+fi
+
+message "Waiting for secrets controller to be available..."
 while :; do
   kubectl get svc sealed-secrets-controller -n kube-system && break
   sleep 15
 done
 
+# Creating/Updating Sealed Secrets -------------------
 "$SCRIPT_DIR/configure-cluster.sh" "$CLUSTER_NAME"
-
-kubectl apply -f "./clusters/$CLUSTER_NAME/ClusterKustomization.yaml"
 
 message "all done!"
