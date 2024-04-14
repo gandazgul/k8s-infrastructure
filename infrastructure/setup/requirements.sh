8d6ab7203b95ecd12b74c543797da0bc5bd9e15b
Carlos Ravelo <carlos.ravelo@ask.com>
2024-04-14 15:54:56 -0400

    updated scripts to use whoami instead of always needing to pass it as a param. Separated KubeSystem from GitRepo source to be able to modify it with a script
    Signed-off-by: Carlos Ravelo <carlos.ravelo@ask.com>

--- infrastructure/setup/requirements.sh
+++ infrastructure/setup/requirements.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 need() {
   if ! command -v "$1" &>/dev/null; then
     echo "Binary '$1' is missing but required"
@@ -18,8 +20,25 @@ pause() {
   echo ""
 }
 
+# make sure all required binaries are installed
 need "kubectl"
 need "flux"
 need "git"
 need "kubeseal"
 need "jq"
+
+# check if have the cluster name, otherwise set it to the current user
+if [ -z ${1+x} ]; then
+  echo -e "Cluster Name was not specified. Assuming \e[1;32m$(whoami)\e[0m as the cluster name."
+  CLUSTER_NAME=$(whoami)
+else
+  CLUSTER_NAME=$1
+fi
+
+# The root of the git repo
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+if [ ! -f "$REPO_ROOT/clusters/$CLUSTER_NAME/secrets.env" ]; then
+  echo "The secrets.env file for $CLUSTER_NAME does not exist. Please create it."
+  exit 1
+fi
